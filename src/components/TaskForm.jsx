250bb14668e017e7b8a2147b5938c350b41a14af
bagusadlan <bagusadlann@gmail.com>
2024-01-30 21:38:52 +0700

    feat(task): add task form component

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,9 +1,55 @@
-function TaskForm() {
+import { useState } from 'react'
+
+function TaskForm({ onAdd }) {
+  const [taskForm, setTaskForm] = useState({
+    title: '',
+    description: ''
+  })
+
+  function handleInputChange(e) {
+    const { name, value } = e.target
+    setTaskForm({
+      ...taskForm,
+      [name]: value
+    })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    onAdd(taskForm)
+  }
+
+  let isSubmitDisabled = !(taskForm.title && taskForm.description)
+
   return (
-    <div>
-      <form>
-        
-      </form>
+    <div className="grow flex items-center justify-around">
+      <div className="mb-16">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="To Do Title"
+            className="mb-2"
+            name="title"
+            value={taskForm.title}
+            onChange={handleInputChange}
+          />
+          <textarea
+            placeholder="Description"
+            className="mb-2"
+            name="description"
+            value={taskForm.description}
+            onChange={handleInputChange}
+          />
+          <button
+            className={`bg-primary w-full py-2 px-4 rounded-2xl text-white${
+              isSubmitDisabled ? ' bg-disabled' : ' bg-primary'
+            }`}
+            disabled={isSubmitDisabled}
+          >
+            Add
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
